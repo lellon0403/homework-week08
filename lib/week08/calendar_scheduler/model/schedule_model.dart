1aafbc2a77584384e3cd371523a3a9a0deac900b
lellon0403 <a67733198@gmail.com>
2025-10-31 21:10:55 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/model/schedule_model.dart
+++ lib/week08/calendar_scheduler/model/schedule_model.dart
@@ -10,5 +10,14 @@ ScheduleModel({
   required this.id,
   required this.content,
   required this.date,
-});
+  required this.startTime,
+  required this.endTime,
+  });
+
+  ScheduleModel.fromJson({// JSON으로부터 모델을 만들어내는 생성자
+  required Map<String, dynamic> json,
+  }) : id = json['id'],
+  content = DateTime.parse(json['date']),
+  date = DateTime.parse(json['date']),
+  startTime
 }
