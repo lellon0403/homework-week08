dcee832f4b789267266234ada1885f1a18fa02c4
lellon0403 <a67733198@gmail.com>
2025-10-31 21:18:25 +0900

    Updated Changes

---
+++ lib/week08/calendar_scheduler/repository/schedule_repository.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
+import 'package:dio/dio.dart';
+
+class ScheduleRepository {
+  final _dio = Dio();
+  final _targetUrl = 'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
+  
+}
