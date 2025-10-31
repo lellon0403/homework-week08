e2f53c1285922d97195c4d70478907ce56e680a5
lellon0403 <a67733198@gmail.com>
2025-10-31 21:06:25 +0900

    Updated Changes

--- lib/week08/calendar_scheduler/main.dart
+++ lib/week08/calendar_scheduler/main.dart
@@ -1,7 +1,7 @@
-import 'package:myapp/week07/calendar_scheduler/screen/home_screen.dart';
+import 'package:myapp/week08/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:myapp/week08/calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
 
 void main() async{
